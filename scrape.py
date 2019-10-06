50497c90fe42b621c165c261c201f4906ac47653
Idkirsch <s195483@student.dtu.dk>
2019-10-06 15:26:02 +0200

    træt

--- scrape.py
+++ scrape.py
@@ -3,13 +3,29 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
 
 
-res = requests.get("https://svoemkbh.kk.dk/hilleroedgade-bad")
-print(res)
+driver=webdriver.Chrome(executable_path=r"C:/Program Files (x86)/chromedriver")
+driver.get("https://svoemkbh.kk.dk/hilleroedgade-bad")
 
-soup = BeautifulSoup(res.content, features="html.parser")
-print(soup)
+noget = driver.find_element_by_name("description")
+nogetAndet = noget.get_attribute("innerHTML")
+print(nogetAndet)
+
+# Wait 5 seconds before scraping 
+# time.sleep(5)
+
+
+# res = requests.get("https://svoemkbh.kk.dk/hilleroedgade-bad")
+# print(res)
+
+
+
+# soup = BeautifulSoup(res.content, features="html.parser")
+# print(soup)
+
 
 # table = soup.find_all('') 
 # df = pd.read_html(str(table))
