20dfbe9cb2adf1a45d6154a4b7c9f3bee59ffd96
Idkirsch <s195483@student.dtu.dk>
2019-10-07 16:34:50 +0200

    kan nu printe antal svømmere

--- scrape.py
+++ scrape.py
@@ -5,14 +5,19 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
+import json
+
 
 
 # driver=webdriver.Chrome(executable_path=r"C:/Program Files (x86)/chromedriver")
 data = requests.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ").content
-print(data)
+# print(data)
 # driver.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ")
 
 # Find parser der kan læse json for at skaffe data
+y = json.loads(data)
+print("antal svømmere:")
+print(y["value"])
 
 
 # noget = driver.find_element_by_name("description")
