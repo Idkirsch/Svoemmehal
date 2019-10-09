052af52f9fa9549bb192727fc5466347617897f7
Idkirsch <s195483@student.dtu.dk>
2019-10-09 13:39:51 +0200

    Jeg har forsøgt at lave et while-loop

--- scrape.py
+++ scrape.py
@@ -8,35 +8,11 @@ import requests
 import json
 
 
-
-# driver=webdriver.Chrome(executable_path=r"C:/Program Files (x86)/chromedriver")
-data = requests.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ").content
-# print(data)
-# driver.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ")
+i = 0
+while i == 0:
+		data = requests.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ").content
 
 # Find parser der kan læse json for at skaffe data
-y = json.loads(data)
-print("antal svømmere: "+str(y["value"]))
-
-
-
-# noget = driver.find_element_by_name("description")
-# nogetAndet = noget.get_attribute("innerHTML")
-# print(nogetAndet)
-
-# Wait 5 seconds before scraping 
-# time.sleep(5)
-
-
-# res = requests.get("https://svoemkbh.kk.dk/hilleroedgade-bad")
-# print(res)
-
-
-
-# soup = BeautifulSoup(res.content, features="html.parser")
-# print(soup)
-
-
-# table = soup.find_all('') 
-# df = pd.read_html(str(table))
-# print(df[0].to_json(orient='records'))
+		y = json.loads(data)
+		print("antal svømmere: "+str(y["value"]))
+		time.sleep(1)
