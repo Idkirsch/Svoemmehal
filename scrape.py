a6e468be3d3064e7f40d358c5a2c835b0bc26658
Karl Kristiansen <kkr@anthillagency.com>
2019-10-09 13:41:48 +0200

    fix

--- scrape.py
+++ scrape.py
@@ -3,16 +3,16 @@
 # import pandas as pd
 import requests
 # from bs4 import BeautifulSoup
-# import time
+import time
 # from selenium import webdriver
 import json
 
 
 i = 0
 while i == 0:
-		data = requests.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ").content
+	data = requests.get("https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ").content
 
 # Find parser der kan læse json for at skaffe data
-		y = json.loads(data)
-		print("antal svømmere: "+str(y["value"]))
-		time.sleep(1)
+	y = json.loads(data)
+	print("antal svømmere: "+str(y["value"]))
+	time.sleep(1)
