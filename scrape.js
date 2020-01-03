f25092004854f6cec48ef026c10d7b7373706601
Idkirsch <s195483@student.dtu.dk>
2020-01-03 16:05:25 +0100

    har forsøgt nogle ændringer

--- scrape.js
+++ scrape.js
@@ -15,8 +15,17 @@ const fetch = require("node-fetch");
 fetch('https://l.vemcount.com/embed/data/W5mOsIMsECIwoYZ')
     .then(res => res.json())
     .then((out) => {
-        console.log('Output: ', out);
+        console.log('Output: ', out.value);
 }).catch(err => console.error(err));
 
 
+// var response = out;
+
+// var div = document.createElement("div");
+// div.innerHTML = "antal svømmere: " + response.value;
+
+// document.getElementsByTagName("body")[0].appendChild(div);
+
 //Næste skridt er at finde ud af hvordan man skriver værdierne fra et json objekt ud i html
+
+
