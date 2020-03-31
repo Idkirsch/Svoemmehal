0cdb80ae8be7bb8b26f43d3b8357415ea60b0ae5
Karl Kristiansen <kkr@anthillagency.com>
2020-03-31 08:22:50 +0200

    haveforening scraper

---
+++ have.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC    
+from selenium.webdriver.common.by import By
+
+import time
+import os
+import glob
+
+from io import TextIOWrapper
+import codecs
+
+import sys
+from datetime import datetime, timedelta
+import re
+
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory' : r'/Users/kkr/Documents/Dev/ida/svoemmehal/output', "directory_upgrade": True}
+chrome_options.add_experimental_option('prefs', prefs)
+chrome_options.add_argument("--start-maximized") # needed for download to specific directory ... 
+
+driver = webdriver.Chrome(executable_path=r'/Users/kkr/chromedriver', options=chrome_options)
+
+def super_get(url):
+    driver.get(url)
+    driver.execute_script("window.onbeforeunload = function() {};")
+
+
+
+firstTime = True
+x = 1
+
+while firstTime: 
+	super_get("http://utterslevdal.123hjemmeside.dk/277767321");
+	assert "Venteliste" in driver.title
+
+
+	fullSource = driver.execute_script("return document.documentElement.innerText")
+	print(fullSource)
+	firstTime = False
+	time.sleep(3)
+	# driver.close()
+	# driver.quit()
+
+while x:
+	super_get("http://utterslevdal.123hjemmeside.dk/277767321");
+	assert "Venteliste" in driver.title
+
+
+	newSource = driver.execute_script("return document.documentElement.innerText")
+
+
+	if newSource != fullSource:
+		print(newSource)
+		print('IT CHANGED')
+		os.system("say \"It changed. Oh my God!\"")
+		x = -1
+
+	else:
+		print('Still the same')
+		# os.system("say \"It changed. Oh my God!\"")
+		time.sleep(10)
