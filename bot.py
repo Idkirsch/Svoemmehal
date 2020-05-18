b4d0ec0c6ac8979e8bfeee52b8782175d9ff32db
Karl Kristiansen <kkr@anthillagency.com>
2020-05-18 20:02:03 +0200

    Checks for nouns

--- bot.py
+++ bot.py
@@ -7,6 +7,10 @@ import tweepy
 import asyncio
 import time
 
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+
+
 
 
 # Initiate the parser
@@ -45,76 +49,91 @@ def checkOxfordForRandomWord(dump):
         # x = 0
         return dump
 
-try:
+word_id = RandomWords().get_random_word()
 
-	word_id = RandomWords().get_random_word()
-	print(word_id)
-	url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
-	r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
-	# print("dump: "+str(dump))
-	
-	dump = r.json()
-	r = str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-	print("r: ", str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]))
-	# x = 0
+word_string = str(word_id)
 
-	word = word_id.lower()
-	first = word[0]
+print(word_string)
+noundict = dictionary.meaning(word_string)
+print(noundict)
 
-	if len(word) > 0 and word.isalpha():
-		if first in ('a', 'e', 'i', 'o', 'u'):  #or `if first in 'aeiou'`
-			anFix = 'n '
-		else:
-			anFix = ' '
+if 'Noun' in noundict:
 
-	# dump = r.json()
-	output = "A human being is like a"+anFix+word_id+": "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-	print(output)
-	os.system("say \""+output+"\"")
+	try:
 
-	# Authenticate to Twitter
-	auth = tweepy.OAuthHandler("V17rp0sFfvBfVnFk4w7UIHZ2v", "EjjK5tpp5VvlKx7GROytYcU4ELAkCFSF7s0XEdP61tqOx8aG0G")
-	auth.set_access_token("1035197003012161538-AopERnlGo3nUjVKXcfmc85VHX7rP6G", "W924j48mPpyOz1GMNMUexn2lFw1L13pu71ZekboOjv8u0")
+		
+		print(word_id)
 
-	# Create API object
-	api = tweepy.API(auth)
+		print (dictionary.meaning("indentation"))
 
-	# Create a tweet
-	api.update_status(output)
+		url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+		r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+		# print("dump: "+str(dump))
+		
+		dump = r.json()
+		r = str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
+		print("r: ", str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]))
+		# x = 0
 
-	# while dump == None:
-	# 	checkOxfordForRandomWord(dump)
-	# 	time.sleep(10)
-	# 	# print("dumpy: "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]))
+		word = word_id.lower()
+		first = word[0]
 
+		if len(word) > 0 and word.isalpha():
+			if first in 'aeiou':  #or `if first in ('a', 'e', 'i', 'o', 'u')`
+				anFix = 'n '
+			else:
+				anFix = ' '
 
+		# dump = r.json()
+		output = "A human being is like a"+anFix+word_id+": "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
+		print(output)
+		os.system("say \""+output+"\"")
 
-	# if r != None:
-	# 	# dump = r.json()
-	# 	output = "A human being is like a "+word_id+": "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-	# 	print("A human being is like a "+word_id+": "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]))
-	# 	os.system("say \""+output+"\"")
+		# Authenticate to Twitter
+		auth = tweepy.OAuthHandler("V17rp0sFfvBfVnFk4w7UIHZ2v", "EjjK5tpp5VvlKx7GROytYcU4ELAkCFSF7s0XEdP61tqOx8aG0G")
+		auth.set_access_token("1035197003012161538-AopERnlGo3nUjVKXcfmc85VHX7rP6G", "W924j48mPpyOz1GMNMUexn2lFw1L13pu71ZekboOjv8u0")
 
-	# 	# Authenticate to Twitter
-	# 	auth = tweepy.OAuthHandler("V17rp0sFfvBfVnFk4w7UIHZ2v", "EjjK5tpp5VvlKx7GROytYcU4ELAkCFSF7s0XEdP61tqOx8aG0G")
-	# 	auth.set_access_token("1035197003012161538-AopERnlGo3nUjVKXcfmc85VHX7rP6G", "W924j48mPpyOz1GMNMUexn2lFw1L13pu71ZekboOjv8u0")
+		# Create API object
+		api = tweepy.API(auth)
 
-	# 	# Create API object
-	# 	api = tweepy.API(auth)
+		# Create a tweet
+		api.update_status(output)
 
-	# 	# Create a tweet
-	# 	api.update_status(output)
-	# else:
-	# 	print("I never got a response")
+		# while dump == None:
+		# 	checkOxfordForRandomWord(dump)
+		# 	time.sleep(10)
+		# 	# print("dumpy: "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]))
 
 
-except Exception as e:
-	print("Fejl: "+str(e))
-	raise
+
+		# if r != None:
+		# 	# dump = r.json()
+		# 	output = "A human being is like a "+word_id+": "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
+		# 	print("A human being is like a "+word_id+": "+str(dump['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]))
+		# 	os.system("say \""+output+"\"")
+
+		# 	# Authenticate to Twitter
+		# 	auth = tweepy.OAuthHandler("V17rp0sFfvBfVnFk4w7UIHZ2v", "EjjK5tpp5VvlKx7GROytYcU4ELAkCFSF7s0XEdP61tqOx8aG0G")
+		# 	auth.set_access_token("1035197003012161538-AopERnlGo3nUjVKXcfmc85VHX7rP6G", "W924j48mPpyOz1GMNMUexn2lFw1L13pu71ZekboOjv8u0")
+
+		# 	# Create API object
+		# 	api = tweepy.API(auth)
+
+		# 	# Create a tweet
+		# 	api.update_status(output)
+		# else:
+		# 	print("I never got a response")
+
+
+	except Exception as e:
+		print("Fejl: "+str(e))
+		raise
+	else:
+		pass
+	finally:
+		pass
 else:
-	pass
-finally:
-	pass
+	print('Not a noun')
 
 
 
